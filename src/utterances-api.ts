622a1a6d770691b76a2d88cb8273eaa9af2548d1
Ivan Jovanovic <ij.smartnet@gmail.com>
2018-03-27 16:21:40 +0200

    utterances api variable can now be set on window.utterancesConfig

--- src/utterances-api.ts
+++ src/utterances-api.ts
@@ -1,2 +1,11 @@
-// export const UTTERANCES_API = 'http://localhost:5000';
-export const UTTERANCES_API = 'https://utterances-oauth.azurewebsites.net';
+// this is to make ts compiler happy
+declare global {
+  interface Window {
+    utterancesConfig: any
+  }
+}
+
+let uc: any = window.utterancesConfig;
+let apiUrl = uc.hasOwnProperty('utterancesApi') ? uc['utterancesApi'] : 'http://localhost:5000';
+
+export const UTTERANCES_API = apiUrl;
