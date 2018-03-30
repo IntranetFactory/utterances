7809241fffcfd66ab54b7c4bc383b71b61f4f33e
Ivan Jovanovic <ij.smartnet@gmail.com>
2018-03-30 16:24:43 +0200

    AT-397 removed branding from timeline component -- removed comment counter from timeline component

--- src/timeline-component.ts
+++ src/timeline-component.ts
@@ -1,29 +1,19 @@
-import { User, Issue, IssueComment } from './github';
+import { User, IssueComment } from './github';
 import { CommentComponent } from './comment-component';
 import { publishResize } from './bus';
 
 export class TimelineComponent {
   public readonly element: HTMLElement;
   private readonly timeline: CommentComponent[] = [];
-  private readonly countAnchor: HTMLAnchorElement;
   private readonly marker: Node;
-  private count: number = 0;
 
   constructor(
-    private user: User | null,
-    private issue: Issue | null
+    private user: User | null
   ) {
     this.element = document.createElement('section');
     this.element.classList.add('timeline');
-    this.element.innerHTML = `
-      <h1 class="timeline-header">
-        <a class="text-link" target="_blank"></a>
-      </h1>`;
-    this.countAnchor = this.element.firstElementChild!.firstElementChild as HTMLAnchorElement;
     this.marker = document.createComment('marker');
     this.element.appendChild(this.marker);
-    this.setIssue(this.issue);
-    this.renderCount();
   }
 
   public setUser(user: User | null) {
@@ -35,27 +25,12 @@ export class TimelineComponent {
     publishResize();
   }
 
-  public setIssue(issue: Issue | null) {
-    this.issue = issue;
-    if (issue) {
-      this.countAnchor.href = issue.html_url;
-    } else {
-      this.countAnchor.removeAttribute('href');
-    }
-  }
-
   public appendComment(comment: IssueComment) {
     const component = new CommentComponent(
       comment,
       this.user ? this.user.login : null);
     this.timeline.push(component);
     this.element.insertBefore(component.element, this.marker);
-    this.count++;
-    this.renderCount();
     publishResize();
-  }
-
-  private renderCount() {
-    this.countAnchor.textContent = `${this.count} Comment${this.count === 1 ? '' : 's'}`;
   }
 }
